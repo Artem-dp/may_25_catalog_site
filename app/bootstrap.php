1f7bac834bf581b386bd11b4bcdfd1e3ffc19d8d
Artem-dp <artemklicenko0@gmail.com>
2025-11-05 13:26:07 +0200

    add class autoloader

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,1 +1,14 @@
 <?php
+define('ROOT_PATH', realpath(__DIR__ . DIRECTORY_SEPARATOR . '..') . DIRECTORY_SEPARATOR);
+
+/**
+ * class autoloader
+ */
+spl_autoload_register(function ($class){
+   $file = ROOT_PATH . str_replace('\\', DIRECTORY_SEPARATOR, $class) . 'php';
+   if (file_exists($file)){
+       include_once $file;
+       return true;
+   }
+   return false;
+});
