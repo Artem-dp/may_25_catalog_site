fd3e83cc9ddcbbca93a7490f5627a158b2401e84
Artem-dp <artemklicenko0@gmail.com>
2025-11-05 18:21:14 +0200

    init router

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,14 +1,22 @@
 <?php
-define('ROOT_PATH', realpath(__DIR__ . DIRECTORY_SEPARATOR . '..') . DIRECTORY_SEPARATOR);
+session_start();
+define('ROOT_PATH', dirname(__DIR__) . DIRECTORY_SEPARATOR);
+define('APP_PATH', ROOT_PATH . 'app' . DIRECTORY_SEPARATOR);
 
 /**
  * class autoloader
  */
 spl_autoload_register(function ($class){
-   $file = ROOT_PATH . str_replace('\\', DIRECTORY_SEPARATOR, $class) . 'php';
+   $file = ROOT_PATH . str_replace('\\', DIRECTORY_SEPARATOR, $class) . '.php';
    if (file_exists($file)){
        include_once $file;
        return true;
    }
    return false;
 });
+
+
+
+$router = new \app\core\Router();
+require_once APP_PATH . 'routes.php';
+$router->init();
