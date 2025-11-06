6d5b3d4f0a73b4749719da01f09164bbd003fb8d
gessshtalt <nackilar897@gmail.com>
2025-11-06 16:38:27 +0100

    controller

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -4,5 +4,24 @@ namespace app\core;
 
 class Controller
 {
+    // Метод для рендера страниц
+    protected function render($view, $data = [])
+    {
+        // Формируем путь к шаблону
+        $viewPath = __DIR__ . '/../../resources/views/' . $view . '.php';
 
+        // Проверяем, существует ли файл шаблона
+        if (!file_exists($viewPath)) {
+            die('View "' . $view . '" not found.');
+        }
+
+        // Превращаем элементы массива в переменные:
+        // ['title' => 'Текст'] станет $title = 'Текст'
+        foreach ($data as $key => $value) {
+            $$key = $value;
+        }
+
+        // Подключаем шаблон
+        require $viewPath;
+    }
 }
