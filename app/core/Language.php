9ba404d49dc10b8347b81928ca5bb6a140e56aeb
gessshtalt <nackilar897@gmail.com>
2025-11-14 14:52:46 +0100

    lang

--- app/core/Language.php
+++ app/core/Language.php
@@ -3,43 +3,26 @@
 namespace app\core;
 
 use app\interfaces\LanguageAwareInterface;
+use app\models\admin\LanguagesModel;
 
 class Language implements LanguageAwareInterface
 {
     public static function getLanguages(): array
     {
-//        $model = new LanguagesModel();
-//        $languages = $model->getAll();
-//
-//        // преобразуем результат в ассоциативный массив вида ['en' => 'English', ...]
-//        $result = [];
-//        foreach ($languages as $lang) {
-//            $result[$lang['code']] = $lang['name'];
-//        }
-
-        return [
-            ['id' => 1, 'code' => 'en', 'name' => 'English'],
-            ['id' => 1, 'code' => 'ru', 'name' => 'Русский'],
-            ['id' => 1, 'code' => 'uk', 'name' => 'Українська']
-        ];
+        $model = new LanguagesModel();
+        return $model->getAll();
     }
 
-    //получаем текущий язык из сессии
     public static function getCurrentLanguage(): string
     {
         if (!isset($_SESSION['language'])) {
-            $_SESSION['language'] = Env::config('DEFAULT_LANG'); // по умолчанию
+            $_SESSION['language'] = Env::config('DEFAULT_LANG');
         }
         return $_SESSION['language'];
     }
 
-    //устанавливаем текущий язык
     public static function setLanguage(string $langCode): void
     {
         $_SESSION['language'] = $langCode;
-    }
-    public static function getDefaultLanguage(): string
-    {
-        return Env::config('DEFAULT_LANG');
     }
 }
