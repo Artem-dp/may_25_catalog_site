ce76c3c8bbf06c4f0c5fc09289aa2b758267f2a0
Artem-dp <artemklicenko0@gmail.com>
2025-11-15 08:04:48 +0200

    add DashboardController documentation

--- app/controllers/admin/DashboardController.php
+++ app/controllers/admin/DashboardController.php
@@ -8,6 +8,10 @@ use app\models\admin\CatalogModel;
 
 class DashboardController extends Controller
 {
+    /**
+     * displaying dashboard
+     * @return void
+     */
     public function index():void
     {
         $this->render('admin/dashboard_template', [
